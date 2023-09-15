6ebe3ae08ee432db852ad075472ec633eb633f1f
omatillo7 <omatillo.khadjaev@gmail.com>
2023-09-15 18:56:26 +0500

    o

--- src/components/Layout/AppCarousel.vue
+++ src/components/Layout/AppCarousel.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <b-carousel class="carousel"
+    <!-- <b-carousel class="carousel"
       id="carousel-fade"
       style="text-shadow: 0px 0px 2px #000;margin-top:30px"
       fade
@@ -26,7 +26,44 @@
           </div>
         </template>
       </b-carousel-slide>
+    </b-carousel> -->
+      <div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;border-radius:50px; margin-top:30px"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <b-carousel-slide style="border-radius:50px">
+        <template #img>
+          <div style="background-color:#45BBD7; border-radius:50px" class="main-carousel-img">
+            <img src="@/assets/product/lightBlue-shoes.png" alt />
+          </div>
+        </template>
+      </b-carousel-slide>
+      <b-carousel-slide style="border-radius:50px">
+        <template #img>
+          <div style="background-color:#EB7B8F;border-radius:50px" class="main-carousel-img">
+            <img src="@/assets/product/mixColor-shoes.png" alt />
+          </div>
+        </template>
+      </b-carousel-slide>
+      <b-carousel-slide style="border-radius:50px">
+        <template #img>
+          <div style="background-color:#68C3DB;border-radius:50px" class="main-carousel-img">
+            <img src="@/assets/product/white-shoes.png" alt />
+          </div>
+        </template>
+      </b-carousel-slide>
     </b-carousel>
+  </div>
   </div>
 </template>
 
@@ -36,17 +73,11 @@ export default {
   comments: { BCarousel, BCarouselSlide },
   data() {
     return {
-      slide: 0,
-      sliding: null
+     
     };
   },
   methods: {
-    onSlideStart() {
-      this.sliding = true;
-    },
-    onSlideEnd() {
-      this.sliding = false;
-    }
+    
   }
 };
 </script>
