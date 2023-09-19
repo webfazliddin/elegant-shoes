67d83dca9b1befbe0a805b8cac2474219af2e084
omatillo7 <omatillo.khadjaev@gmail.com>
2023-09-19 21:10:56 +0500

    o

---
+++ src/components/Layout/AppCategory.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="mainCategory">
+      <div>
+         <img src="@/assets/product/mixColor-shoes.png" alt />
+      </div>
+      <div>
+        <img src="@/assets/product/mixColor-shoes.png" alt />
+      </div>
+      <div>
+        <img src="@/assets/product/mixColor-shoes.png" alt />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { BCarousel, BCarouselSlide } from "bootstrap-vue";
+export default {
+  comments: { BCarousel, BCarouselSlide },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+<style>
+.mainCategory{
+    display: flex;
+    margin-top: 40px;
+    justify-content: center;
+    gap: 380px;
+}
+.mainCategory div{
+    flex-wrap: wrap;
+    width: 160px;
+    height: auto;
+   border-radius: 25px;
+   box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+}
+.mainCategory img{
+    width: 140px;
+}
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,6 +4,8 @@
 
     <AppCarousel />
 
+    <AppCategory />
+
     <router-view></router-view>
 
     <AppFooter />
@@ -16,6 +18,7 @@
 import AppHeader from "../components/Layout/AppHeader.vue";
 import AppFooter from "../components/Layout/AppFooter.vue";
 import AppCarousel from "../components/Layout/AppCarousel.vue";
+import AppCategory from "../components/Layout/AppCategory.vue";
 
 export default {
   
@@ -24,6 +27,7 @@ export default {
     AppHeader,
     AppFooter,
     AppCarousel,
+    AppCategory
   },
 };
 </script>
