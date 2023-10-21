161fd900b22f569e0f6a11ecd06cc0a4afb7c1cd
omatillo7 <omatillo.khadjaev@gmail.com>
2023-10-22 00:46:28 +0500

    o

--- src/data/about/index.vue
+++ src/data/about/index.vue
@@ -3,34 +3,20 @@
     <div>
       <img class="mainImg" src="@/assets/aboutImg/mainImg.jpg" />
     </div>
-    <!-- <div>
-      <b-card
-        title="Founder"
-        img-src="https://picsum.photos/500/400/?image=25"
-        img-alt="Image"
-        img-top
-        tag="article"
-        style="max-width: 16rem;margin-left: auto;margin-right: auto;"
-        class="mb-2 mt-4"
-      >
-        <h6>Turg'unov Abdurashid</h6>
-      </b-card>
-    </div>-->
     <div>
       <b-card
         no-body
         class="overflow-hidden mt-4 Bcard"
-        style="max-width: 540px;margin-left: auto;margin-right: auto;"
+        style="max-width: 440px; margin-left: auto; margin-right: auto"
       >
         <b-row no-gutters>
           <b-col md="6">
-            <b-card-img src="https://picsum.photos/400/400/?image=20" alt="Image" class="rounded-0"></b-card-img>
+              <img class="founderImg" src="@/assets/aboutImg/founder.jpg" />
           </b-col>
           <b-col md="6">
             <b-card-body :title="$t('Founder')">
-              <b-card-text>
-                This is a wider card with supporting text as a natural lead-in to additional content.
-                This content is a little bit longer.
+              <b-card-text class="fullname">
+                Turg'unov Abdurashid
               </b-card-text>
             </b-card-body>
           </b-col>
@@ -39,56 +25,46 @@
     </div>
     <div class="PersonCard">
       <div>
-        <b-card no-body class="overflow-hidden mt-4" style="max-width: 540px;">
+        <b-card no-body class="overflow-hidden mt-4 Bcard" style="max-width: 540px">
           <b-row no-gutters>
-            <b-col md="6">
-              <b-card-img
-                src="https://picsum.photos/400/400/?image=20"
-                alt="Image"
-                class="rounded-0"
-              ></b-card-img>
-            </b-col>
-            <b-col md="6">
-              <b-card-body :title="$t('Founder')">
-                <b-card-text>
-                  This is a wider card with supporting text as a natural lead-in to additional content.
-                  This content is a little bit longer.
-                </b-card-text>
-              </b-card-body>
-            </b-col>
-          </b-row>
+          <b-col md="6">
+              <img class="founderImg" src="@/assets/aboutImg/founder.jpg" />
+          </b-col>
+          <b-col md="6">
+            <b-card-body :title="$t('Buxgalter')">
+              <b-card-text class="fullname">
+                Turg'unov Abdurashid
+              </b-card-text>
+            </b-card-body>
+          </b-col>
+        </b-row>
         </b-card>
       </div>
       <div>
-        <b-card no-body class="overflow-hidden mt-4" style="max-width: 540px;">
-          <b-row no-gutters>
-            <b-col md="6">
-              <b-card-img
-                src="https://picsum.photos/400/400/?image=20"
-                alt="Image"
-                class="rounded-0"
-              ></b-card-img>
-            </b-col>
-            <b-col md="6">
-              <b-card-body :title="$t('Founder')">
-                <b-card-text>
-                  This is a wider card with supporting text as a natural lead-in to additional content.
-                  This content is a little bit longer.
-                </b-card-text>
-              </b-card-body>
-            </b-col>
-          </b-row>
+        <b-card no-body class="overflow-hidden mt-4 Bcard" style="max-width: 540px">
+         <b-row no-gutters>
+          <b-col md="6">
+              <img class="founderImg" src="@/assets/aboutImg/founder.jpg" />
+          </b-col>
+          <b-col md="6">
+            <b-card-body :title="$t('Ish boshqaruvchi')">
+              <b-card-text class="fullname">
+                Turg'unov Abdurashid
+              </b-card-text>
+            </b-card-body>
+          </b-col>
+        </b-row>
         </b-card>
       </div>
     </div>
-    <div style="margin-top:20px"></div>
+    <div style="margin-top: 20px"></div>
   </div>
 </template>
 
 <script>
 export default {
   components: {},
-  data() {}
+  data() {},
 };
 </script>
 
@@ -97,9 +73,17 @@ export default {
   margin-top: 20px;
   border-radius: 15px;
 }
-.PersonCard{
-    display:flex;
-    justify-content: space-between;
+.PersonCard {
+  display: flex;
+  justify-content: space-between;
+}
+.founderImg {
+ width: 200px;
+ height: 200px;
+}
+.fullname{
+  font-size: 20px;
+  font-weight: 400;
 }
 @media screen and (max-width: 768px) {
   .mainImg {
@@ -107,12 +91,19 @@ export default {
     height: auto;
     border-radius: 5px;
   }
-  .PersonCard{
-    /* width: 300px;
-    height: auto; */
-    display:flex;
-     flex-wrap: wrap;
-    justify-content: space-between;
-}
+  .PersonCard {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  .Bcard{
+    width: 300px;
+    height: 300px;
+    display: flex;
+  }
+  .Bcard img{
+    width: 310px;
+    height: 310px;
+  }
 }
 </style>
