a0ec379bee74e0aa3bfe3471f1f1a747cf7cf7fe
omatillo7 <omatillo.khadjaev@gmail.com>
2023-10-22 19:37:12 +0500

    o

--- src/components/Cards/CarouselCard.vue
+++ src/components/Cards/CarouselCard.vue
@@ -1,82 +1,81 @@
 <template>
-  <div>
-    <b-carousel
-      id="carousel-1"
-      v-model="slide"
-      :interval="4000"
-      controls
-      indicators
-      background="#ababab"
-      img-width="1024"
-      img-height="480"
-      style="
-        text-shadow: 1px 1px 2px #333;
-        border-radius: 15px;
-        margin-top: 30px;
-      "
-      class="carousel"
-      @sliding-start="onSlideStart"
-      @sliding-end="onSlideEnd"
-    >
-      <b-carousel-slide style="border-radius: 15px"
+      <div>
+        <b-carousel
+          id="carousel-1"
+          v-model="slide"
+          :interval="4000"
+          controls
+          indicators
+          background="#ababab"
+          img-width="1024"
+          img-height="480"
+          style="
+            text-shadow: 1px 1px 2px #333;
+            border-radius: 15px;
+            margin-top: 30px;
+          "
+          class="carousel"
+          @sliding-start="onSlideStart"
+          @sliding-end="onSlideEnd"
         >
-        <template #img>
-          <div
-            style="background-color: #45bbd7; border-radius: 15px"
-            class="main-carousel-img"
-          >
-            <img src="@/assets/product/lightBlue-shoes.png" alt />
-          </div>
-        </template>
-      </b-carousel-slide>
-      <b-carousel-slide style="border-radius: 15px">
-        <template #img>
-          <div
-            style="background-color: #eb7b8f; border-radius: 15px"
-            class="main-carousel-img"
-          >
-            <img src="@/assets/product/mixColor-shoes.png" alt />
-          </div>
-        </template>
-      </b-carousel-slide>
-      <b-carousel-slide style="border-radius: 15px">
-        <template #img>
-          <div
-            style="background-color: #68c3db; border-radius: 15px"
-            class="main-carousel-img"
-          >
-            <img src="@/assets/product/white-shoes.png" alt />
-          </div>
-        </template>
-      </b-carousel-slide>
-    </b-carousel>
-  </div>
-</template>
+          <b-carousel-slide style="border-radius: 15px">
+            <template #img>
+              <div
+                style="background-color: #45bbd7; border-radius: 15px"
+                class="main-carousel-img"
+              >
+                <img src="@/assets/product/lightBlue-shoes.png" alt />
+              </div>
+            </template>
+          </b-carousel-slide>
+          <b-carousel-slide style="border-radius: 15px">
+            <template #img>
+              <div
+                style="background-color: #eb7b8f; border-radius: 15px"
+                class="main-carousel-img"
+              >
+                <img src="@/assets/product/mixColor-shoes.png" alt />
+              </div>
+            </template>
+          </b-carousel-slide>
+          <b-carousel-slide style="border-radius: 15px">
+            <template #img>
+              <div
+                style="background-color: #68c3db; border-radius: 15px"
+                class="main-carousel-img"
+              >
+                <img src="@/assets/product/white-shoes.png" alt />
+              </div>
+            </template>
+          </b-carousel-slide>
+        </b-carousel>
+      </div>
+  </template>
   
   <script>
-import { BCarousel, BCarouselSlide } from "bootstrap-vue";
-export default {
-  comments: { BCarousel, BCarouselSlide },
-  data() {
-    return {};
-  },
-  methods: {},
-};
-</script>
+  import { BCarousel, BCarouselSlide } from "bootstrap-vue";
+  export default {
+    comments: { BCarousel, BCarouselSlide },
+    data() {
+      return {};
+    },
+    methods: {},
+  };
+  </script>
   <style>
-.main-carousel-img {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.carousel img {
-  height: 440px;
-}
-
-@media screen and (max-width: 768px) {
-  .carousel img {
-    height: 200px;
+  .main-carousel-img {
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
-}
-</style>
+  .carousel img {
+    height: 440px;
+  }
+  
+  @media screen and (max-width: 768px) {
+    .carousel img {
+      height: 200px;
+    }
+  }
+  </style>
   
